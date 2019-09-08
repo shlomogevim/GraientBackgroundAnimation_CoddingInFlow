dad640e6ff41cdd84f6e0088c07c86533a5c8592
shlomogevim <shlomo.gevim@gmail.com>
2019-09-08 09:56:37 +0300

    Initial commit

---
+++ app/src/main/java/com/example/graientbackgroundanimation_coddinginflow/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.graientbackgroundanimation_coddinginflow
+
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val animationDrawable=layout.background as AnimationDrawable
+        animationDrawable.setEnterFadeDuration(2000)
+        animationDrawable.setExitFadeDuration(4000)
+        animationDrawable.start()
+    }
+}
